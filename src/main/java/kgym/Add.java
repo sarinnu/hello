a9bdb04f479164ede9117bc87eb022e980420e32
pytpp <pytpp@192.168.11.11>
2020-06-22 14:45:46 +0900

    add edit

--- src/main/java/kgym/Add.java
+++ src/main/java/kgym/Add.java
@@ -16,17 +16,14 @@ public class Add {
 	@RequestMapping(value="/add",method=RequestMethod.GET)
 	public String add(HttpSession session,@RequestParam("code") String code,Model model) {
 		
-//		セッションからカートに追加する商品のデータを取り出す
 		String user=session.getAttribute("user").toString();
 	    UserData data=(UserData)session.getAttribute(user);
 		ProductDataBeans item=(ProductDataBeans)session.getAttribute(code);
 
-//      ユーザーのカートデータを更新
 		ArrayList<ProductDataBeans> items=data.getItem();
 		items.add(item);
 	    data.setItem(items);
 	    
-//      追加した商品の名前をHTMLに送信
 		model.addAttribute("name",item.getProductName());
 		return "add";
 	}
